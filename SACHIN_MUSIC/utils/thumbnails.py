ca8908ce9e02f975206b921977fa7a68b6d2d828
TaitanX2 <taitanrowshan21@gmail.com>
2024-12-26 12:25:29 +0530

    Update thumbnails.py

--- SACHIN_MUSIC/utils/thumbnails.py
+++ SACHIN_MUSIC/utils/thumbnails.py
@@ -116,7 +116,7 @@ async def get_thumb(videoid):
     circle_thumbnail = circle_thumbnail.resize((400, 400))
     circle_position = (120, 160)
     background.paste(circle_thumbnail, circle_position, circle_thumbnail)
-    background.paste(Image.open("SACHIN_MUSIC/assets/bg.jpeg")
+    background.paste(Image.open("SACHIN_MUSIC/assets/bg.jpeg"))
 
     text_x_position = 565
 
