0656bba70463c8ac4b6c6747ea26853aab9dd1d8
DilwarHosen <105662694+DilwarHosen@users.noreply.github.com>
2024-12-27 17:44:21 +0530

    Update start.py

--- SACHIN_MUSIC/plugins/bot/start.py
+++ SACHIN_MUSIC/plugins/bot/start.py
@@ -26,9 +26,9 @@ from config import BANNED_USERS
 from strings import get_string
 
 NEXIO = [
-    "https://files.catbox.moe/jakpk9.jpg",
-    "https://files.catbox.moe/a8vac3.jpg",
-    "https://files.catbox.moe/7q2xop.jpg",
+    "https://files.catbox.moe/gd7nt0.jpg",
+    "https://files.catbox.moe/gd7nt0.jpg",
+    "https://files.catbox.moe/gd7nt0.jpg",
 ]
 
 
