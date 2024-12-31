972a74445fb289e399a642f48a0e503162636afe
DilwarHosen <105662694+DilwarHosen@users.noreply.github.com>
2024-12-31 16:00:17 +0530

    Add files via upload

--- SACHIN_MUSIC/help/buttons.py
+++ SACHIN_MUSIC/help/buttons.py
@@ -35,29 +35,61 @@ class BUTTONS(object):
     
     MBUTTON = [
                 [
-            InlineKeyboardButton("єxᴛʀᴧ", callback_data="MANAGEMENT_BACK HELP_25"),
+            InlineKeyboardButton("ᴧɪ | ᴄʜᴧᴛɢᴘᴛ", callback_data="TOOL_BACK HELP_01"),
         ],
         [
             InlineKeyboardButton("ʙᴧη", callback_data="MANAGEMENT_BACK HELP_14"),
             InlineKeyboardButton("ᴋɪᴄᴋ", callback_data="MANAGEMENT_BACK HELP_15"),
+        ],
+        [
             InlineKeyboardButton("ϻυᴛє", callback_data="MANAGEMENT_BACK HELP_16"),
+            InlineKeyboardButton("єxᴛʀᴧ", callback_data="MANAGEMENT_BACK HELP_25"),
         ],
         [
             InlineKeyboardButton("ᴘɪη", callback_data="MANAGEMENT_BACK HELP_17"),
+            InlineKeyboardButton("ғιʟᴛєяѕ", callback_data="MANAGEMENT_BACK HELP_27"),
+        ],
+        [
             InlineKeyboardButton("sᴛᴧғғ", callback_data="MANAGEMENT_BACK HELP_18"),
             InlineKeyboardButton("sєᴛ-υᴘ", callback_data="MANAGEMENT_BACK HELP_19"),
         ],
         [
             InlineKeyboardButton("ᴢσϻʙɪє", callback_data="MANAGEMENT_BACK HELP_20"),
             InlineKeyboardButton("ɢᴧϻє", callback_data="MANAGEMENT_BACK HELP_21"),
-            InlineKeyboardButton("ɪϻᴘσsᴛєʀ", callback_data="MANAGEMENT_BACK HELP_22"),
         ],
         [
+            InlineKeyboardButton("ɪϻᴘσsᴛєʀ", callback_data="MANAGEMENT_BACK HELP_22"),
             InlineKeyboardButton("sɢ", callback_data="MANAGEMENT_BACK HELP_23"),
+        ],
+        [
             InlineKeyboardButton("ᴛʀ", callback_data="MANAGEMENT_BACK HELP_24"),
             InlineKeyboardButton("ɢʀᴧᴘʜ", callback_data="MANAGEMENT_BACK HELP_26"),
         ],
         [
+            InlineKeyboardButton("sєᴧʀᴄʜ", callback_data="MANAGEMENT_BACK HELP_02"),
+            InlineKeyboardButton("ᴛᴛs", callback_data="MANAGEMENT_BACK HELP_03"),
+        ],
+        [
+            InlineKeyboardButton("ɪηғσ", callback_data="MANAGEMENT_BACK HELP_04"),
+            InlineKeyboardButton("ғσηᴛ", callback_data="MANAGEMENT_BACK HELP_05"),
+        ],
+        [
+            InlineKeyboardButton("ϻᴧᴛʜ", callback_data="MANAGEMENT_BACK HELP_06"),
+            InlineKeyboardButton("ᴛᴧɢᴧʟʟ", callback_data="MANAGEMENT_BACK HELP_07"),
+        ],
+        [
+            InlineKeyboardButton("ɪϻᴧɢє", callback_data="MANAGEMENT_BACK HELP_08"),
+            InlineKeyboardButton("ʜᴧsᴛᴧɢ", callback_data="MANAGEMENT_BACK HELP_09"),
+        ],
+        [
+            InlineKeyboardButton("sᴛɪᴄᴋєʀs", callback_data="MANAGEMENT_BACK HELP_10"),
+            InlineKeyboardButton("ғυη", callback_data="MANAGEMENT_BACK HELP_11"),
+        ],
+        [
+            InlineKeyboardButton("ǫυσᴛʟʏ", callback_data="MANAGEMENT_BACK HELP_12"),
+            InlineKeyboardButton("ᴛ-ᴅ", callback_data="MANAGEMENT_BACK HELP_13"),
+        ],
+        [   
             InlineKeyboardButton("⌯ ʙᴧᴄᴋ ⌯", callback_data=f"MAIN_CP"), 
         ]
         ]
@@ -88,7 +120,7 @@ class BUTTONS(object):
             InlineKeyboardButton("ϻᴧηᴧɢєϻєηᴛ", callback_data="MANAGEMENT_CP"),
         ],
         [
-            InlineKeyboardButton("ᴛσσʟs", callback_data="TOOL_CP"),
+           ## InlineKeyboardButton("ᴛσσʟs", callback_data="TOOL_CP"),
             InlineKeyboardButton("ᴧʙσυт", callback_data="MAIN_BACK HELP_ABOUT"),
         ],
         [
