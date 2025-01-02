9babd437bdbd238fed18d9ef0b8734cecb059d38
DilwarHosen <105662694+DilwarHosen@users.noreply.github.com>
2025-01-02 19:21:27 +0530

    Update play.py

--- SACHIN_MUSIC/utils/inline/play.py
+++ SACHIN_MUSIC/utils/inline/play.py
@@ -64,7 +64,7 @@ def stream_markup_timer(_, chat_id, played, dur):
          InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
         ],
         [
-         InlineKeyboardButton(text="[🇮🇳] Aʟᴏɴᴇ", user_id=config.OWNER_ID),
+     #    InlineKeyboardButton(text="[🇮🇳] Aʟᴏɴᴇ", user_id=config.OWNER_ID),
          InlineKeyboardButton(text="• Uᴘᴅᴀᴛᴇs •", url=config.SUPPORT_CHANNEL),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -75,7 +75,7 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-         InlineKeyboardButton(text="[🇮🇳] Aʟᴏɴᴇ", user_id=config.OWNER_ID),
+      #   InlineKeyboardButton(text="[🇮🇳] Aʟᴏɴᴇ", user_id=config.OWNER_ID),
          InlineKeyboardButton(text="• Uᴘᴅᴀᴛᴇs •", url=config.SUPPORT_CHANNEL),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
