d61729615cf6c0fe81e6dc80dc2abb4f0318768b
DilwarHosen <105662694+DilwarHosen@users.noreply.github.com>
2025-01-03 08:47:09 +0530

    Update help.py

--- SACHIN_MUSIC/plugins/bot/help.py
+++ SACHIN_MUSIC/plugins/bot/help.py
@@ -45,8 +45,8 @@ async def helper_private(
         language = await get_lang(update.chat.id)
         _ = get_string(language)
         keyboard = help_pannel(_)
-        await update.reply_photo(
-            photo=START_IMG_URL,
+        await update.reply_video(
+            video="https://files.catbox.moe/njw9xa.mp4",
             caption=_["help_1"].format(SUPPORT_CHAT),
             reply_markup=keyboard,
         )
